df414fa5a1303e2f915298633a91137e97044a33
jeckt <steven@sleepyproductions.com>
2018-07-26 21:04:33 +0000

    Update model tests to all use Decimal for the amount

--- core/tests/test_models.py
+++ core/tests/test_models.py
@@ -10,30 +10,35 @@ class ExpenseModelTest(TestCase):
     def test_default_expense(self):
         expense = Expense()
         self.assertEqual(expense.description, '')
-        self.assertEqual(expense.amount, 0)
+        self.assertEqual(expense.amount, Decimal('0'))
 
     def test_cannot_save_expense_with_no_description(self):
-        expense = Expense.objects.create(description='', amount=5.5)
+        expense = Expense.objects.create(description='',
+                        amount=Decimal('5.5')
+        )
         with self.assertRaises(ValidationError):
             expense.save()
             expense.full_clean()
 
     def test_cannot_save_expense_with_zero_amount(self):
         expense = Expense.objects.create(description='expense',
-                                         amount=0)
+                        amount=Decimal('0')
+        )
         with self.assertRaises(ValidationError):
             expense.save()
             expense.full_clean()
 
     def test_cannot_save_expense_with_negative_amount(self):
         expense = Expense.objects.create(description='expense',
-                                         amount=Decimal('-0.01'))
+                        amount=Decimal('-0.01')
+        )
         with self.assertRaises(ValidationError):
             expense.save()
             expense.full_clean()
 
     def test_can_save_expense_with_positive_amount(self):
         expense = Expense.objects.create(description='expense',
-                                         amount=Decimal('0.01'))
+                        amount=Decimal('0.01')
+        )
         expense.save()
         expense.full_clean() # should not raise
