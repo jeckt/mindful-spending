5f30107849797bc5b92965ad4d5552209ab73e7c
jeckt <steven.nguyen89@gmail.com>
2018-07-29 17:36:20 +0000

    Update site settings for production and ignore env variables

--- mindfulspending/settings.py
+++ mindfulspending/settings.py
@@ -19,14 +19,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'vjiwl0-%$kf#5lbq)!s6046pk7q-e98@@r)&md5*mdyvf5k&wk'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
+if 'DJANGO_DEBUG_FALSE' in os.envion:
+    DEBUG = False
+    SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+    ALLOWED_HOSTS = [os.environ['SITENAME']]
+else:
+    DEBUG = True
+    SECRET_KEY = 'vjiwl0-%$kf#5lbq)!s6046pk7q-e98@@r)&md5*mdyvf5k&wk'
+    ALLOWED_HOSTS = []
 
 # Application definition
 
