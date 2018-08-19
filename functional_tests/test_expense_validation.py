26f74e644eaa01b5e014632ed21c71ca6445fc3a
jeckt <steven@sleepyproductions.com>
2018-08-19 23:59:09 +0000

    refactor validation FT to use home page object

--- functional_tests/test_expense_validation.py
+++ functional_tests/test_expense_validation.py
@@ -1,4 +1,7 @@
 from .base import FunctionalTest
+from .page import HomePage
+
+from datetime import date
 
 class ExpenseValidationTest(FunctionalTest):
 
@@ -9,8 +12,12 @@ class ExpenseValidationTest(FunctionalTest):
         # clicks the log button before even adding an amount
         # to his burger for lunch!
         self.browser.get(self.live_server_url)
-        self.get_description_input_box().send_keys('Burger for lunch')
-        self.get_submit_input_button().click()
+
+        expense_date = date.today()
+
+        home_page = HomePage(self)
+        home_page.get_description_input_box().send_keys('Burger for lunch')
+        home_page.get_submit_input_button().click()
 
         # The browser intercepts the request, and does not
         # add the empty expense to the list and provides
@@ -22,31 +29,32 @@ class ExpenseValidationTest(FunctionalTest):
         # He then starts entering an amount for the new item
         # But forgets he doesn't need to put a minus to show
         # that it is an expense!
-        self.get_amount_input_box().clear()
-        self.get_amount_input_box().send_keys("-8.35")
-        self.get_submit_input_button().click()
+        home_page.get_amount_input_box().clear()
+        home_page.get_amount_input_box().send_keys("-8.35")
+        home_page.get_submit_input_button().click()
         self.wait_for(lambda: self.browser.find_element_by_css_selector(
             '#id_amount:invalid'
         ))
 
         # He removes the minus in the amount and tries again.
-        self.get_amount_input_box().clear()
-        self.get_amount_input_box().send_keys("8.35")
+        home_page.get_amount_input_box().clear()
+        home_page.get_amount_input_box().send_keys("8.35")
         self.wait_for(lambda: self.browser.find_element_by_css_selector(
             '#id_amount:valid'
         ))
 
         # This time it works!
-        self.get_submit_input_button().click()
-        self.wait_for_row_in_list_table('Burger for lunch',
-                                        '$8.35',
-                                        self.expense_date)
+        home_page.get_submit_input_button().click()
+        home_page.wait_for_row_in_list_table(
+            'Burger for lunch',
+            '8.35',
+            expense_date)
 
         # That was difficult! On the drive home from work
         # he decided to buy a Maccas Large Soda and this
         # time remembers to input the amount!
-        self.get_amount_input_box().send_keys("1.09")
-        self.get_submit_input_button().click()
+        home_page.get_amount_input_box().send_keys("1.09")
+        home_page.get_submit_input_button().click()
 
         # Unfortunately he forgot to put the description
         # and gets another error!
@@ -56,16 +64,18 @@ class ExpenseValidationTest(FunctionalTest):
 
         # He starts typing the description and the error
         # disappears
-        self.get_description_input_box().send_keys('Maccas Large Soda')
+        home_page.get_description_input_box().send_keys('Maccas Large Soda')
         self.wait_for(lambda: self.browser.find_element_by_css_selector(
             '#id_amount:valid'
         ))
 
         # He now can log the second expense!
-        self.get_submit_input_button().click()
-        self.wait_for_row_in_list_table('Burger for lunch',
-                                        '$8.35',
-                                        self.expense_date)
-        self.wait_for_row_in_list_table('Maccas Large Soda',
-                                        '$1.09',
-                                        self.expense_date)
+        home_page.get_submit_input_button().click()
+        home_page.wait_for_row_in_list_table(
+            'Burger for lunch',
+            '8.35',
+            expense_date)
+        home_page.wait_for_row_in_list_table(
+            'Maccas Large Soda',
+            '1.09',
+            expense_date)
