5f84d97431ad8a677f0d83bfcbf8df49e4a37dfb
jeckt <steven@sleepyproductions.com>
2018-08-19 23:39:19 +0000

    Finish refactor of edit FT to use page objects

--- functional_tests/page.py
+++ functional_tests/page.py
@@ -1,3 +1,4 @@
+from .base import wait
 
 class Page(object):
 
@@ -10,6 +11,22 @@ class HomePage(Page):
         self.test.browser.find_element_by_id('id_edit').click()
         return EditPage(self.test)
 
+    def get_description_input_box(self):
+        return self.test.browser.find_element_by_id('id_description')
+
+    def get_amount_input_box(self):
+        return self.test.browser.find_element_by_id('id_amount')
+
+    def get_submit_input_button(self):
+        return self.test.browser.find_element_by_id('id_submit')
+
+    @wait
+    def wait_for_row_in_list_table(self, description, amount, expense_date):
+        rows = self.test.browser.find_elements_by_tag_name('td')
+        self.test.assertIn(description, [row.text for row in rows])
+        self.test.assertIn(f'${amount}', [row.text for row in rows])
+        self.test.assertIn(expense_date.strftime('%d-%b-%Y'), [row.text for row in rows])
+
 class EditPage(Page):
 
     def update_date(self, new_date):
@@ -17,7 +34,7 @@ class EditPage(Page):
             "//input[@name='date']"
         )
         input_.clear()
-        input_.send_keys(new_date)
+        input_.send_keys(new_date.strftime('%Y-%m-%d'))
 
     def update_amount(self, new_amount):
         input_ = self.test.browser.find_element_by_xpath(
@@ -26,6 +43,29 @@ class EditPage(Page):
         input_.clear()
         input_.send_keys(new_amount)
 
+    def update_expense(self):
+        self.test.browser.find_element_by_id(f'id_edit_1').click()
+
     def go_to_home_page(self):
         self.test.browser.find_element_by_id('id_home').click()
         return HomePage(self.test)
+
+    @wait
+    def wait_for_multiple_rows_in_table(self, description, amount,
+                                         expense_date, count):
+
+        input_rows = self.test.browser.find_elements_by_tag_name('input')
+        input_values = [input_row.get_attribute('value') for input_row in
+                        input_rows]
+        self.test.assertEqual(
+            len([ value for value in input_values if value == expense_date.strftime('%Y-%m-%d')]),
+            count * 2
+        )
+        self.test.assertEqual(
+            len([ value for value in input_values if value == description]),
+            count
+        )
+        self.test.assertEqual(
+            len([ value for value in input_values if value == amount]),
+            count
+        )
--- functional_tests/test_edit_expenses.py
+++ functional_tests/test_edit_expenses.py
@@ -12,19 +12,25 @@ class ExpenseEditTest(FunctionalTest):
         self.browser.get(self.live_server_url)
         home_page = HomePage(self)
 
+        description = 'Beers after work'
+        amount = '37.10'
+        amount_with_tip = '47.10'
+        expense_date = date.today()
+        new_expense_date = expense_date + timedelta(days=-1)
+
         # She logs her first expense. She normally doesn't
         # have breakfast and today was no exception, so
         # she logs last night's team drinks.
-        expense_date = date.today() + timedelta(days=-1)
-        date_string = expense_date.strftime('%Y-%m-%d')
-        self.get_description_input_box().send_keys('Beers after work')
-        self.get_amount_input_box().send_keys('37.10')
-        self.get_submit_input_button().click()
+        home_page.get_description_input_box().send_keys(description)
+        home_page.get_amount_input_box().send_keys(amount)
+        home_page.get_submit_input_button().click()
 
         # She sees the expense in the list!
-        self.wait_for_row_in_list_table('Beers after work',
-                                        '$37.10',
-                                        self.expense_date)
+        home_page.wait_for_row_in_list_table(
+            description,
+            amount,
+            expense_date
+        )
 
         # Unfortunately it doesn't have the right date!
         # She proceeds to the edit page.
@@ -32,35 +38,44 @@ class ExpenseEditTest(FunctionalTest):
 
         # Here she sees that she is able to change the
         # date of the expense and does so.
-        self.wait_for_multiple_rows_in_edit_table('Beers after work',
-            '37.10',
-            date.today().strftime('%Y-%m-%d'),
+        edit_page.wait_for_multiple_rows_in_table(
+            description,
+            amount,
+            expense_date,
             1
         )
-        edit_page.update_date(date_string)
-        self.wait_for(lambda:
-                      self.browser.find_element_by_id('id_edit_1').click())
+
+        new_expense_date = date.today() + timedelta(days=-1)
+        edit_page.update_date(new_expense_date)
+        edit_page.update_expense()
 
         # The page refreshes and she sees the expense date
         # has updated to what she expected
-        self.wait_for_multiple_rows_in_edit_table('Beers after work',
-                                                   '37.10',
-                                                   date_string, 1)
+        edit_page.wait_for_multiple_rows_in_table(
+            description,
+            amount,
+            new_expense_date,
+            1
+        )
 
         # Thinking again she forgot about the tip! So
         # she goes ahead and updates that as well.
-        edit_page.update_amount('47.10')
-        self.wait_for(lambda:
-                      self.browser.find_element_by_id('id_edit_1').click())
+        edit_page.update_amount(amount_with_tip)
+        edit_page.update_expense()
 
         # Again she sees that has updated...
-        self.wait_for_multiple_rows_in_edit_table('Beers after work',
-                                                   '47.10',
-                                                   date_string, 1)
+        edit_page.wait_for_multiple_rows_in_table(
+            description,
+            amount_with_tip,
+            new_expense_date,
+            1
+        )
 
         # Satisfied she returns to the home page
         # and see the changes have persisted.
         edit_page.go_to_home_page()
-        self.wait_for_row_in_list_table('Beers after work',
-                                        '$47.10',
-                                        expense_date.strftime('%d-%b-%Y'))
+        home_page.wait_for_row_in_list_table(
+            description,
+            amount_with_tip,
+            new_expense_date
+        )
